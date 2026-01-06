fa1a74b5cc7aca7c2508de1cd51f0875b47f95f3
xyhtac <73641145+xyhtac@users.noreply.github.com>
2026-01-06 14:35:40 +0100

    Update app.js

--- swarm/app.js
+++ swarm/app.js
@@ -39,6 +39,10 @@ location /${swarmPath}/ {
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_ssl_verify off;
+	proxy_ssl_session_reuse off;
+	proxy_ssl_server_name on;
+	proxy_ssl_name $host;
 
     proxy_pass http://${info.ip}:${info.port}/${swarmPath}/;
 }

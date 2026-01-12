c4c53fffab556a8e3e94ba710b42e7e3f951f13d
xyhtac <73641145+xyhtac@users.noreply.github.com>
2026-01-12 16:12:51 +0100

    Update app.js

--- swarm/app.js
+++ swarm/app.js
@@ -39,7 +39,7 @@ location /${swarmPath}/ {
 	  proxy_ssl_server_name on;
 	  proxy_ssl_name $host;
 
-    proxy_pass https://${info.ip}:${info.port}/${swarmPath}/;
+    proxy_pass https://${info.ip}:${info.port}/;
 }
 `).join('\n');
 }

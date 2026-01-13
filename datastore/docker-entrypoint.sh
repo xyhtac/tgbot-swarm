6ca85e35ff2b58d23881af759cf958ccaa6581b2
xyhtac <73641145+xyhtac@users.noreply.github.com>
2026-01-13 01:14:27 +0100

    redo db

--- datastore/docker-entrypoint.sh
+++ datastore/docker-entrypoint.sh
@@ -42,7 +42,8 @@ fi
 
 # Start MariaDB in background
 
-mysqld --port=$DB_PORT --user=mysql --datadir=/var/lib/mysql &
+#mysqld --port=$DB_PORT --user=mysql --datadir=/var/lib/mysql &
+exec mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking=0 --bind-address=0.0.0.0 --port=$DB_PORT &
 DB_PID=$!
 
 # mysqld_safe --datadir=/var/lib/mysql &
